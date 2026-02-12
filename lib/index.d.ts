a7a0ca1fe0bd77dad00264601a3d0cfd4f728690
Andrei Elkin <andreyyolkin@gmail.com>
2026-02-12 13:11:35 +0100

    chore: remove Vue 2 support and simplify package
    Remove all Vue 2 related code, scripts, and CLI tools. The package now only supports Vue 3

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,11 +3,6 @@ declare const isVue2: boolean
 declare const isVue3: boolean
 declare const Vue2: any
 declare const install: (vue?: any) => void
-/** 
- * @deprecated To avoid bringing in all the tree-shakable modules, this API has been deprecated. Use `Vue2` or named exports instead.
- * Refer to https://github.com/vueuse/vue-demi/issues/41
- */
-declare const V: typeof Vue
 
 export function set<T>(target: any, key: any, val: T): T
 export function del(target: any, key: any): void
